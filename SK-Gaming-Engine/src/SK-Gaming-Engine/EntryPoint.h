783555edb38ad17d22e78ada3c9a4963221cbe1f
StK1729 <stefan.kraljevic@gmail.com>
2020-03-25 03:08:27 +0100

    Added Premake

--- SK-Gaming-Engine/src/SK-Gaming-Engine/EntryPoint.h
+++ SK-Gaming-Engine/src/SK-Gaming-Engine/EntryPoint.h
@@ -7,7 +7,7 @@ extern SK_Gaming_Engine::Application* SK_Gaming_Engine::CreateApplication();
 int main(int argc, char** argv)
 {
 	SK_Gaming_Engine::Log::Init();
-	SKGE_CORE_TRACE("Hello {0}", "Stefan");
+	SKGE_CORE_TRACE("Hello {0}", "Stefan Kraljevic");
 	SKGE_CLIENT_INFO("Initialized client logger.");
 	SK_Gaming_Engine::Application* app = SK_Gaming_Engine::CreateApplication();
 	app->Run();
